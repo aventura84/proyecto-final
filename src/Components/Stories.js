7fe2aec47ad770e30305ed1d3bb08cb46ece8dbc
Antonio <ventura_antonio@hotmail.com>
2022-10-26 17:20:25 +0200

    ultimo

---
+++ src/Components/Stories.js
@@ -0,0 +1,15 @@
+import"../Styles/stories.scss";
+import HorizontalScroll from "react-scroll-horizontal";
+import Story from"./Story";
+function Stories(){
+
+    return<div className="stories">
+            <HorizontalScroll className="scroll"reverseScroll={true}>
+                <Story/>
+                <Story/><Story/>
+                </HorizontalScroll>
+                </div>;
+    
+}
+
+export default Stories
---
+++ src/data/users.js
@@ -0,0 +1,16 @@
+const users =[
+
+{
+    id:1,
+    name:"Leanne Graham",
+    username:"Bret",
+},
+{
+    id:2,
+    name:"Ervin Howell"
+    username:"Antonette",
+}
+
+];
+
+export default users;
