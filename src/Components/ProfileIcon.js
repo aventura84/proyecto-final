eee313fe08bd70733434b1c3a204bb1b977e3e03
Antonio <ventura_antonio@hotmail.com>
2022-10-28 23:25:09 +0200

    eliminacion

--- src/Components/ProfileIcon.js
+++ src/Components/ProfileIcon.js
@@ -1,28 +1,27 @@
-import"./profileIcon.css";
+import "./profileIcon.css";
 
-function profileIcon(props){
-    const{ iconSize, storyBorder,image } = props;
+function profileIcon(props) {
+  const { iconSize, storyBorder, image } = props;
 
-    function getRandomInt(min, max){
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random()*(max-min +1))+min;
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  let randomId = getRandomInt(1, 70);
 
-
-
-
-    }
-    let randomId = getRandomInt(1,70);
-
-    let profileImage = image 
-    ? image: 
-    'https://i.pravatar.cc/150?img=${randomId}';
-return(
-    <div className={storyBorder?"storyBorder":""}>
-        <img className={'profileIcon ${iconSize}'} src={profileImage} alt="profile"/>
-
+  let profileImage = image
+    ? image
+    : "https://i.pravatar.cc/150?img=${randomId}";
+  return (
+    <div className={storyBorder ? "storyBorder" : ""}>
+      <img
+        className={"profileIcon ${iconSize}"}
+        src={profileImage}
+        alt="profile"
+      />
     </div>
-)
+  );
 }
 
 export default profileIcon;
