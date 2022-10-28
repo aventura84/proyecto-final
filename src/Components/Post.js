7c26e3e7751b8fce12fb37dd6e3ea2ecad4beaeb
Antonio <ventura_antonio@hotmail.com>
2022-10-28 16:37:49 +0200

    todo

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,78 +1,30 @@
-import React, { useState,useEffect } from "react";
-import "./Post.css";
-//import{} from Post
-//import Avatar from "@material-ui/core/Avatar";
-//function Post({ username, caption, imageUrl }) {
-//const [comments,setComments]= useState ([]);
-//useEffect(()=>{
-// let unsubcribe;
-//  if(postId){
-//unsubcribe=
-//.collection("posts")
-//.doc(postId)
-//.collection("comments")
-//.orderBy('timestamp','desc')
-//.onSnapshot((snapshot)=>{
-//setComments(snapshot.docs.map((doc)))
-//});
+import React, { useState, useEffect } from "react";
+import { postComments } from "../data/comments.js";
+import CommentForm from "./CommentForm.js";
+import Comments from "./CommentForm.js";
 
- // }
-  
-  //return()=>{
-   // unsubcribe ();
+function Post({ username, caption, imageUrl }) {
+  const [comments, setComments] = useState([]);
 
-//};
-//},[postId]);
+  return (
+    <div className="">
+      <img
+        className="post_avatar"
+        alt="RafehQazi"
+        src="http://picsum.photos/50/50" />
+      <h3>{username}</h3>
 
-//const postComments = (event)=>{
-//event.preventDefault();
-//}
+      {/*header->avatar + username*/}
+      <img className="post__image" src={imageUrl} alt="" />
 
-//  return (
-//    <div className="post">''
- //     <div className="post__header">
-//        </div>
- //       <Avatar
-  //        className="post_avatar"
-  //        alt="RafehQazi"
-//          src="static/images/avatar/1.jpg"
- //      <h3>{username}</h3>
+      <h4 className="post__text">
+        <strong>{username}</strong> {caption}
+      </h4>
+      {/*username+caption*/}
+      {/* <Comments comments={postComments} /> */}
+      <CommentForm />
+    </div>
+  )
+}
 
-//   {/*header->avatar + username*/}
-//      <img className="post__image" src={imageUrl} alt="" />
-
- //     <h4 className="post__text">
-  //      <strong>{username}</strong> {caption}
- //     </h4>
-//     {/*username+caption*/}
-//<div className="post__comments">
-//{postComments.map((commments)=>(
- // <p>
-//    <strong>{comment.username}</strong>{comment.text}
- // </p>
-//) )}
-//</div>
-//      <form className="post__commentBox">
-//       <input
-//        className="post__input"
-//       type="text"
-//        placeholder="Add a comment..."
-//       value={comment}
- //       onChange={(e)=>setComment(e.target.value)}
-//       />
-//        <buttton
-//        className="post__button"
-//disabled={!comment}
-//type="submit"
-//onClick={postComment}
-//>
-//Post
-//</buttton>
-//</form>
-//      )
-//   </div>
-// )
-//}
-
-//export default Post;
-//
+export default Post
--- src/Components/ProfileIcon.js
+++ src/Components/ProfileIcon.js
@@ -1,8 +1,27 @@
-import"../styles/profileIcon.css";
+import"./profileIcon.css";
 
-function profileIcon(){
+function profileIcon(props){
+    const{ iconSize, storyBorder,image } = props;
+
+    function getRandomInt(min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random()*(max-min +1))+min;
+
+
+
+
+    }
+    let randomId = getRandomInt(1,70);
+
+    let profileImage = image 
+    ? image: 
+    'https://i.pravatar.cc/150?img=${randomId}';
 return(
-    <div className="profileIcon"></div>
+    <div className={storyBorder?"storyBorder":""}>
+        <img className={'profileIcon ${iconSize}'} src={profileImage} alt="profile"/>
+
+    </div>
 )
 }
 
