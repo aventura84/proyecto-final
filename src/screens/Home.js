9abadfbbb447e270c150e837af814f0f85c6097a
Antonio <ventura_antonio@hotmail.com>
2022-10-29 20:06:47 +0200

    actualizacion

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,23 @@
-import Posts from "../Components/Posts.js";
-import Stories from "../Components/Stories.js";
+import Posts from "../components/Posts";
+import { Postlist } from "../components/Postlist";
+import { AuthContext } from "../context/AuthContext";
+import Stories from "../components/Stories";
 
 function HomeScreen() {
-    return <div>
-        <Stories />
-        <Posts />
+  const { posts, loading, error } = Posts();
+
+  if (loading) return <p>loading posts</p>;
+  if (error) return <ErrorMessage message={error} />;
+
+  console.log(posts);
+  return (
+    <div>
+      <Postlist posts={posts} />
+      <Stories />
+      <Posts />
+      {user ? <p>aquí formulario de envío de post</p> : null}
     </div>
+  );
 }
 
 export default HomeScreen;
