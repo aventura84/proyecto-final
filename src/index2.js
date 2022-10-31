94596e18b6c7e0a962bca7fe7dc93a8a7c29e000
Antonio <ventura_antonio@hotmail.com>
2022-10-31 17:57:51 +0100

    like

--- src/index2.js
+++ src/index2.js
@@ -142,3 +142,18 @@ export const deletePostService = async ({ id, token }) => {
     throw new Error(json.message);
   }
 };
+
+//export const deletePostService = async ({ id, token }) => {
+//  const response = await fetch(`${process.env.REACT_APP_BACKEND}/post/${id}`, {
+//    method: "DELETE",
+//   headers: {
+//     Authorization: token,
+//    },
+//  });
+
+//const json = await response.json();
+
+//  if (!response.ok) {
+//   throw new Error(json.message);
+// }
+//};
